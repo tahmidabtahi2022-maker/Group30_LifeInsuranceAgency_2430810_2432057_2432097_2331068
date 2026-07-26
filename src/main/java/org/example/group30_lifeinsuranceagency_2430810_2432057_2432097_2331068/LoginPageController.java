772fdf3810be9e1d49312e05a32233864ec5df05
tahmidabtahi2022 <tahmid.abtahi2022@gmail.com>
2026-07-26 21:18:04 +0600

    loginpage

---
+++ src/main/java/org/example/group30_lifeinsuranceagency_2430810_2432057_2432097_2331068/LoginPageController.java
@@ -0,0 +1,25 @@
+package org.example.group30_lifeinsuranceagency_2430810_2432057_2432097_2331068;
+
+public class LoginPageController
+{
+    @javafx.fxml.FXML
+    private ComboBox selectUserTypeCB;
+    @javafx.fxml.FXML
+    private PasswordField enterPasswordTF;
+    @javafx.fxml.FXML
+    private Label feedbackLabel;
+    @javafx.fxml.FXML
+    private TextField enterEmailAddressTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void registerNewAccountOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loginButtonOnAction(ActionEvent actionEvent) {
+    }
+}
