be35658ab7a826e60744f64365e06361cc1b58ec
jam <2432057@iub.edu.bd>
2026-07-28 11:37:12 +0600

    design

--- src/main/java/Jamiul_Huda/InsuranceAgentControllers/InsuranceAgentDashboardController.java
+++ src/main/java/Jamiul_Huda/InsuranceAgentControllers/InsuranceAgentDashboardController.java
@@ -2,66 +2,62 @@ package Jamiul_Huda.InsuranceAgentControllers;
 
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 
 public class InsuranceAgentDashboardController {
-    @javafx.fxml.FXML
-    private TableColumn clientIdColumn;
-    @javafx.fxml.FXML
-    private Button searchButton;
-    @javafx.fxml.FXML
-    private Label renewalsDueLabel;
-    @javafx.fxml.FXML
-    private Label policyCountValueLabel;
-    @javafx.fxml.FXML
-    private Label totalClientsLabel;
     @javafx.fxml.FXML
     private Button logoutButton;
     @javafx.fxml.FXML
     private Button messagesButton;
     @javafx.fxml.FXML
-    private TableColumn statusColumn;
-    @javafx.fxml.FXML
-    private Label pendingInteractionsLabel;
-    @javafx.fxml.FXML
     private Button clientPortfolioButton;
-    @javafx.fxml.FXML
-    private TableColumn pendingInteractionColumn;
     @javafx.fxml.FXML
     private Label dashboardTitleLabel;
     @javafx.fxml.FXML
-    private TableColumn policyCountColumn;
-    @javafx.fxml.FXML
-    private TextField searchClientTextField;
-    @javafx.fxml.FXML
     private Label agentNameLabel;
-    @javafx.fxml.FXML
-    private TableColumn renewalDeadlineColumn;
     @javafx.fxml.FXML
     private Button pendingRenewalsButton;
     @javafx.fxml.FXML
-    private TableColumn clientNameColumn;
-    @javafx.fxml.FXML
     private Label statusLabel;
-    @javafx.fxml.FXML
-    private Label totalClientsValueLabel;
-    @javafx.fxml.FXML
-    private Label policyCountLabel;
-    @javafx.fxml.FXML
-    private Label clientPortfolioTitleLabel;
     @javafx.fxml.FXML
     private Button dashboardButton;
     @javafx.fxml.FXML
     private Button settingsButton;
-    @javafx.fxml.FXML
+    @FXML
+    private TableColumn clientIdColumn;
+    @FXML
+    private Button searchButton;
+    @FXML
+    private Label renewalsDueLabel;
+    @FXML
+    private Label policyCountValueLabel;
+    @FXML
+    private Label totalClientsLabel;
+    @FXML
+    private TableColumn statusColumn;
+    @FXML
+    private Label pendingInteractionsLabel;
+    @FXML
+    private TableColumn pendingInteractionColumn;
+    @FXML
+    private TableColumn policyCountColumn;
+    @FXML
+    private TextField searchClientTextField;
+    @FXML
+    private TableColumn renewalDeadlineColumn;
+    @FXML
+    private TableColumn clientNameColumn;
+    @FXML
+    private Label totalClientsValueLabel;
+    @FXML
+    private Label policyCountLabel;
+    @FXML
+    private Label clientPortfolioTitleLabel;
+    @FXML
     private Label pendingInteractionsValueLabel;
-    @javafx.fxml.FXML
+    @FXML
     private Label renewalsDueValueLabel;
-    @javafx.fxml.FXML
+    @FXML
     private TableView clientTableView;
 }
