b4789358b81f9753ffa70f9a9a5b6ea87fc12cd4
jam <2432057@iub.edu.bd>
2026-07-28 14:01:09 +0600

    WTF

--- src/main/java/Jamiul_Huda/InsuranceAgentControllers/PolicyApplicationController.java
+++ src/main/java/Jamiul_Huda/InsuranceAgentControllers/PolicyApplicationController.java
@@ -1,4 +1,36 @@
 package Jamiul_Huda.InsuranceAgentControllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class PolicyApplicationController {
+    @javafx.fxml.FXML
+    private Button searchButton;
+    @javafx.fxml.FXML
+    private TextField riskBaselineTextField;
+    @javafx.fxml.FXML
+    private TextField clientIdTextField;
+    @javafx.fxml.FXML
+    private TextField nomineeNameTextField;
+    @javafx.fxml.FXML
+    private TextField applicationStatusTextField;
+    @javafx.fxml.FXML
+    private Label notificationLabel;
+    @javafx.fxml.FXML
+    private Button calculateRiskButton;
+    @javafx.fxml.FXML
+    private TextField customerStatusTextField;
+    @javafx.fxml.FXML
+    private Button clearButton;
+    @javafx.fxml.FXML
+    private Button submitProposalButton;
+    @javafx.fxml.FXML
+    private ComboBox insurancePackageComboBox;
+    @javafx.fxml.FXML
+    private TextField sumAssuredTextField;
+    @javafx.fxml.FXML
+    private ComboBox healthCategoryComboBox;
 }
