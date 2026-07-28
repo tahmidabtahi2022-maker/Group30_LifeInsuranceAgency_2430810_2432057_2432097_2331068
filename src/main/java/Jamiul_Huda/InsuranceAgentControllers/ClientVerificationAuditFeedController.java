234fe3b5470d59b662fc54435c68b84f732d95fc
jam <2432057@iub.edu.bd>
2026-07-28 20:07:38 +0600

    goal 5

--- src/main/java/Jamiul_Huda/InsuranceAgentControllers/ClientVerificationAuditFeedController.java
+++ src/main/java/Jamiul_Huda/InsuranceAgentControllers/ClientVerificationAuditFeedController.java
@@ -6,7 +6,6 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-
 public class ClientVerificationAuditFeedController {
     @javafx.fxml.FXML
     private TableColumn clientIdColumn;
